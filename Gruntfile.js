94492d30f00b29e1c157fc707340f4ddf1eee56e
Nacho Gil <Ignacio.Gil@akqa.com>
2015-05-21 22:16:45 +0100

    Grunt Usemin to build css and js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,45 @@
 // ./Gruntfile.js
 module.exports = function(grunt) {
-    "use strict";
+    'use strict';
 
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+ 
+        copy: {
+            html: {
+                src: 'src/index.html',
+                dest: 'dist/index.html'
+            },
+            php: {
+                src: 'src/mailer.php',
+                dest: 'dist/mailer.php'
+            },
+            fonts: {
+                expand: true, 
+                cwd: 'src/',
+                src: 'font/**',
+                dest: 'dist/'
+            },
+            images: {
+                expand: true, 
+                cwd: 'src/',
+                src: 'img/**',
+                dest: 'dist/'
+            }
+        },
+
+        useminPrepare: {
+            html: 'src/index.html',
+            options: {
+                dest: 'dist'
+            }
+        },
+
+        usemin: {
+            html: ['dist/index.html']
+        },
 
         connect: {
             server: {
@@ -16,7 +51,13 @@ module.exports = function(grunt) {
 
     });
 
-    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
-
-    grunt.registerTask('default', ['connect:server']);
+    grunt.registerTask('default', [
+        'copy',
+        'useminPrepare',
+        'concat',
+        'uglify',
+        'cssmin',
+        'usemin',
+        'connect:server'
+    ]);
 };
